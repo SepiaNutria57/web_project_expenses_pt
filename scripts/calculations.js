3fa787df9e2963d812faebaf981ebc192960b4b6
SepiaNutria57 <felipenogueira812@gmail.com>
2026-07-29 11:18:51 -0300

    Add budget calculation logic and include necessary scripts in index.html

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,1 +1,12 @@
-// test
+let budgetValue = 0;
+let totalExpensesValue = 0;
+
+let expenseEntries = [
+  ["groceries", 33],
+  ["restaurants", 50],
+  ["transport", 12],
+  ["home", 70],
+  ["subscriptions", 20],
+  ["groceries", 28],
+  ["subscriptions", 12],
+];
