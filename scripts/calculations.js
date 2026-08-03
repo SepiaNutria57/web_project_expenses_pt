e2b6172d0004a7f0cc1e76fe38ac2af166be80a9
SepiaNutria57 <felipenogueira812@gmail.com>
2026-08-03 12:01:57 -0300

    Update budget initialization and enhance expense calculations

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,4 +1,4 @@
-let budgetValue = 0;
+let budgetValue = 250;
 let totalExpensesValue = 0;
 let balanceColor = "green";
 
@@ -12,29 +12,42 @@ let expenseEntries = [
   ["subscriptions", 12],
 ];
 
-for (expense in expenseEntries){
-  totalExpensesValue =+ expenseEntries[expense][1];
+for (expense in expenseEntries) {
+  totalExpensesValue = +expenseEntries[expense][1];
 }
 
-function calculateAverageExpense(){
+function calculateAverageExpense() {
   let totalAverage = totalExpensesValue / expenseEntries.length;
   return totalAverage;
 }
 
-function calculateBalance(){
+function calculateBalance() {
   let totalBalance = budgetValue - totalExpensesValue;
   return totalBalance;
 }
 
-function updateBalanceColor(){
-  if (totalBalance <= 0){
+function updateBalanceColor() {
+  if (totalBalance <= 0) {
     balanceColor = "red";
-  } else if (totalBalance < budgetValue*0.25){
+  } else if (totalBalance < budgetValue * 0.25) {
     balanceColor = "orange";
   }
 }
 
-function calculateCategoryExpenses(){
+function calculateCategoryExpenses() {
   //um for/while que pega o nome de uma categoria, checa todas as instâncias dela com um if e soma a despesa à um valor específico
-  //algo como: if expenseEntries[i] === category, total =+ expenseEntries[i][1]
+  //algo como: if expenseEntries[i][0] === category, total =+ expenseEntries[i][1]
+
+  let totalCategoryExpenses = 0;
+
+  for (const expense of expenseEntries) { //para cada despesa em expenseEntries
+    while (i < expenseEntries.length) { // e enquanto o índice for menor que o tamanho da lista
+      if (expenseEntries[i][0] === expense) { // se o nome da categoria da despesa for igual ao nome da categoria que estamos verificando
+         //o certo seria criar uma nova array aninhada...
+         //usar aquele método de array filter() para filtrar as despesas por categoria e depois somar os valores
+         //ou a
+      }
+      i++;
+    }
+  }
 }
